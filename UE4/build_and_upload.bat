ccd0c673aa2a69960afc5bef3fea3c32e1b995f9
Vincent Mahnke <accounts@vimaster.de>
2015-04-19 00:22:09 +0200

    ~ Made the project git-submodule compatible
    Conflicts:
    	UE4/build_and_upload.bat
    	Unity5/build_and_upload.bat

--- UE4/build_and_upload.bat
+++ UE4/build_and_upload.bat
@@ -10,19 +10,19 @@ for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set currentdate=%%c-%%a-%%b
 
 
 :: Create builds
-CALL "%ue4path%/4.7/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -rocket -nocompile -installed -nop4 -project="%~dp0/../0_unreal/TBS.uproject" -cook -allmaps -stage -archive -archivedirectory="%~dp0/../../builds/automated/%currentdate%_%commithash%" -package -clientconfig=Development -ue4exe="UE4Editor-Cmd.exe" -clean -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -utf8output -NoCompile
+CALL "%ue4path%/4.7/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -rocket -nocompile -installed -nop4 -project="%~dp0/../../../0_unreal/TBS.uproject" -cook -allmaps -stage -archive -archivedirectory="%~dp0/../../../../builds/automated/%currentdate%_%commithash%" -package -clientconfig=Development -ue4exe="UE4Editor-Cmd.exe" -clean -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -utf8output -NoCompile
 
 :: Zip them up
-"C:/Program Files (x86)/7-Zip/7z.exe" a -tzip "%~dp0/../../builds/automated/recent/recent.zip" "%~dp0/../../builds/automated/%currentdate%_%commithash%"
+"C:/Program Files (x86)/7-Zip/7z.exe" a -tzip "%~dp0/../../../../builds/automated/recent/recent.zip" "%~dp0/../../../../builds/automated/%currentdate%_%commithash%"
 
 
 :: Generate info.txt
 @echo off
-@echo Build created: %currentdate%> "%~dp0/../../builds/automated/recent/info.txt"
-@echo SHA-1 Hash of commit: %commithash%>> "%~dp0/../../builds/automated/recent/info.txt"
+@echo Build created: %currentdate%> "%~dp0/../../../../builds/automated/recent/info.txt"
+@echo SHA-1 Hash of commit: %commithash%>> "%~dp0/../../../../builds/automated/recent/info.txt"
 
 :: Add info.txt to zip-files
-"C:/Program Files (x86)/7-Zip/7z.exe" a -tzip "%~dp0/../../builds/automated/recent/recent.zip" "%~dp0/../../builds/automated/recent/info.txt"
+"C:/Program Files (x86)/7-Zip/7z.exe" a -tzip "%~dp0/../../../../builds/automated/recent/recent.zip" "%~dp0/../../../../builds/automated/recent/info.txt"
 
 :: Establish ftp-connection
 ftp -s:%~dp0/credentials.dat
