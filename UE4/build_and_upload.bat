00354074e24ec743de8df6b3fc1fdee17211a511
Vincent Mahnke <accounts@vimaster.de>
2015-05-14 16:15:32 +0200

    ~ Fixed trailing-slash errors occurring on some systems

--- UE4/build_and_upload.bat
+++ UE4/build_and_upload.bat
@@ -10,23 +10,23 @@ for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set currentdate=%%c-%%a-%%b
 
 
 :: Create builds
-CALL "%ue4path%/4.7/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -rocket -nocompile -installed -nop4 -project="%~dp0/../../../0_unreal/TBS.uproject" -cook -allmaps -stage -archive -archivedirectory="%~dp0/../../../../builds/automated/%currentdate%_%commithash%" -package -clientconfig=Development -ue4exe="UE4Editor-Cmd.exe" -clean -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -utf8output -NoCompile
+:: CALL "%ue4path%/4.7/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -rocket -nocompile -installed -nop4 -project="%~dp0../../../0_unreal/TBS.uproject" -cook -allmaps -stage -archive -archivedirectory="%~dp0/../../../../builds/automated/%currentdate%_%commithash%" -package -clientconfig=Development -ue4exe="UE4Editor-Cmd.exe" -clean -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -utf8output -NoCompile
 
 :: Copy to temporary folder to prevent "too long filename"-errors
-robocopy "%~dp0/../../../../builds/automated/%currentdate%_%commithash%" "C:/tmp/abu" /MIR /log:backup_log.txt
+robocopy "%~dp0../../../../builds/automated/%currentdate%_%commithash%" "C:/tmp/abu" /MIR /log:backup_log.txt
 
 :: Zip them up
-"%7zippath%/7z.exe" a -tzip "%~dp0/../../../../builds/automated/recent/recent.zip" "C:/tmp/abu"
+"%SEVENZIPPATH%/7z.exe" a -tzip "%~dp0../../../../builds/automated/recent/recent.zip" "C:/tmp/abu"
 
 rmdir "C:/tmp/abu" /s /q
 
 :: Generate info.txt
 @echo off
-@echo Build created: %currentdate%> "%~dp0/../../../../builds/automated/recent/info.txt"
-@echo SHA-1 Hash of commit: %commithash%>> "%~dp0/../../../../builds/automated/recent/info.txt"
+@echo Build created: %currentdate%> "%~dp0../../../../builds/automated/recent/info.txt"
+@echo SHA-1 Hash of commit: %commithash%>> "%~dp0../../../../builds/automated/recent/info.txt"
 
 :: Add info.txt to zip-files
-"%7zippath%/7z.exe" a -tzip "%~dp0/../../../../builds/automated/recent/recent.zip" "%~dp0/../../../../builds/automated/recent/info.txt"
+"%SEVENZIPPATH%/7z.exe" a -tzip "%~dp0../../../../builds/automated/recent/recent.zip" "%~dp0../../../../builds/automated/recent/info.txt"
 
 :: Establish ftp-connection
 ftp -s:%~dp0/credentials.dat
