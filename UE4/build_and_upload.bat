c9fa9e17b9c05cbcebfe43f8ed33fa6cf8a308fc
Vincent Mahnke <accounts@vimaster.de>
2015-05-15 14:56:39 +0200

    ~ Uncommented actual build-command

--- UE4/build_and_upload.bat
+++ UE4/build_and_upload.bat
@@ -10,7 +10,7 @@ for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set currentdate=%%c-%%a-%%b
 
 
 :: Create builds
-:: CALL "%ue4path%/4.7/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -rocket -nocompile -installed -nop4 -project="%~dp0../../../0_unreal/TBS.uproject" -cook -allmaps -stage -archive -archivedirectory="%~dp0/../../../../builds/automated/%currentdate%_%commithash%" -package -clientconfig=Development -ue4exe="UE4Editor-Cmd.exe" -clean -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -utf8output -NoCompile
+CALL "%ue4path%/4.7/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -rocket -nocompile -installed -nop4 -project="%~dp0../../../0_unreal/TBS.uproject" -cook -allmaps -stage -archive -archivedirectory="%~dp0/../../../../builds/automated/%currentdate%_%commithash%" -package -clientconfig=Development -ue4exe="UE4Editor-Cmd.exe" -clean -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -utf8output -NoCompile
 
 :: Copy to temporary folder to prevent "too long filename"-errors
 robocopy "%~dp0../../../../builds/automated/%currentdate%_%commithash%" "C:/tmp/abu" /MIR /log:backup_log.txt
