78a224d3b5d43ad088e797c43da9458bc28633b2
Vincent Mahnke <accounts@vimaster.de>
2015-07-03 06:52:40 +0200

    ~ Paths now match UE4.8 and TBS-wrapper

--- UE4/build_and_upload.bat
+++ UE4/build_and_upload.bat
@@ -9,27 +9,30 @@ for /f %%i in ('git rev-parse HEAD') do set commithash=%%i
 for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set currentdate=%%c-%%a-%%b)
 
 
-:: Create builds
-CALL "%ue4path%/4.7/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -rocket -nocompile -installed -nop4 -project="%~dp0../../../0_unreal/TBS.uproject" -cook -allmaps -stage -archive -archivedirectory="%~dp0/../../../../builds/automated/%currentdate%_%commithash%" -package -clientconfig=Shipping -ue4exe=UE4Editor-Cmd.exe -clean -pak -prereqs -targetplatform=Win32 -build -UseDebugParamForEditorExe -utf8output -NoCompile
+:: Create package
+CALL "%ue4path%/4.8/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -rocket -compile -installed -nop4 -project="%~dp0../../../0_unreal/wrapper/TBS.uproject" -cook -allmaps -stage -archive -archivedirectory="%~dp0/../../../../builds/automated/%currentdate%_%commithash%" -package -clientconfig=Debug -ue4exe="UE4Editor-Cmd.exe" -clean -pak -prereqs -nodebuginfo -platform=Win64 -build -utf8output
+
 
 :: Copy to temporary folder to prevent "too long filename"-errors
 robocopy "%~dp0../../../../builds/automated/%currentdate%_%commithash%" "C:/tmp/abu" /MIR /log:backup_log.txt
+
 
 :: Zip them up
 "%SEVENZIPPATH%/7z.exe" a -tzip "%~dp0../../../../builds/automated/recent/recent.zip" "C:/tmp/abu"
 
 rmdir "C:/tmp/abu" /s /q
 
+
 :: Generate info.txt
 @echo off
 @echo Build created: %currentdate%> "%~dp0../../../../builds/automated/recent/info.txt"
 @echo SHA-1 Hash of commit: %commithash%>> "%~dp0../../../../builds/automated/recent/info.txt"
 
+
 :: Add info.txt to zip-files
 "%SEVENZIPPATH%/7z.exe" a -tzip "%~dp0../../../../builds/automated/recent/recent.zip" "%~dp0../../../../builds/automated/recent/info.txt"
+
 
 :: Establish ftp-connection
 ftp -s:%~dp0/credentials.dat
  
-:: Move back to start folder
-cd %~dp0
